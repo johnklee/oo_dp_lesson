e60c7e411b439d49be9e43a6de4437d2b53fae38
selinahuang <selinahuang@google.com>
2021-07-08 12:35:03 +0800

    my solution for #1

--- homework04/alg_1910_sol.py
+++ homework04/alg_1910_sol.py
@@ -1,8 +1,9 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        i, pl = s.find(part), len(part)
-        while i >= 0:
-            s = s[0:i] + s[i+pl:]
-            i = s.find(part)
-            
-        return s
+        while s:
+            i, pl = s.find(part), len(part)
+            while i >= 0:
+              s = s[0:i] + s[i+pl:]
+              i = s.find(part)
+            return s
+        return ''
--- homework04/alg_1913.py
+++ homework04/alg_1913.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 from typing import List
-
+import pdb
 
 class Solution:
     # 1913. Maximum Product Difference Between Two Pairs
     # https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
     def maxProductDifference(self, nums: List[int]) -> int:
-        sorted_nums = sorted(nums, reverse=True)
-        return sorted_nums[0] * sorted_nums[1] - sorted_nums[-1] * sorted_nums[-2]
-
+        if len(nums) <= 1:
+          return 0
+        else:
+          sorted_nums = sorted(nums, reverse=True)
+          return sorted_nums[0] * sorted_nums[1] - sorted_nums[-1] * sorted_nums[-2]
 
 if __name__ == '__main__':
   sol = Solution()
+
+  #pdb.set_trace()
   for nums, ans in [
       ([5,6,2,7,4], 34),
       ([4,2,5,9,7,4,8], 64),
